234f79dae7e08bd47f986f88c869fb0f7b0c7c12
kkew3 <kps6326@hotmail.com>
2023-08-22 18:17:11 +0800

    Minor code refactoring.

--- pythonx/obsidian/ftdetect.py
+++ pythonx/obsidian/ftdetect.py
@@ -5,11 +5,16 @@ import vim
 
 
 def reload_syntax_if_under_obsidian_vault():
+    if get_obv_basedir():
+        vim.command('let b:under_obsidian_vault = 1')
+        vim.command('syntax on')
+
+
+def get_obv_basedir():
     path = vim.current.buffer.name
     while path != os.path.dirname(path):
         path = os.path.dirname(path)
         if ('.obsidian' in os.listdir(path)
                 and os.path.isdir(os.path.join(path, '.obsidian'))):
-            vim.command('let b:under_obsidian_vault = 1')
-            vim.command('syntax on')
-            break
+            return path
+    return ''
